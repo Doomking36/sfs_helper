5dc44ba9a73a1ab338b089a9cb12121afa719aca
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:11:09 -0600

    Update and rename template.sh to make.sh

---
+++ builds/make.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+env CC="/mnt/bin/tcc -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" LD="echo" ./configure --prefix="/" --disable-nls --disable-rpath \
+    --bindir=/bin --sbindir=/bin --libexecdir=/bin --without-gnu-ld
+
+make -j($nproc)
+make DESTDIR="/mnt/" install
+make distclean
