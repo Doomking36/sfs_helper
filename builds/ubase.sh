940dd76b3eb1af64e0631a706b0a097aff7c47c8
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:07:46 -0600

    Update ubase.sh

--- builds/ubase.sh
+++ builds/ubase.sh
@@ -4,6 +4,6 @@ env CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE" \
     CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
-    RANLIB="echo" make -e -j2
+    RANLIB="echo" make -e -j($nproc)
 make -e PREFIX="/" DESTDIR="/mnt/" install
 make clean
