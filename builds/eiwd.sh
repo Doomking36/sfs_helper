76976cbea8e369e24e532c44039890f7465a482e
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:48:01 -0600

    Update and rename template.sh to eiwd.sh

---
+++ builds/eiwd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+env LD="echo" CC="/src/gcc-toolchain/bin/gcc -Os -pipe -march=native -s -static -g0" \
+    ./configure --prefix=/ --libexecdir=/bin --bindir=/bin --sbindir=/bin \
+    --disable-systemd-service --disable-dbus-policy --disable-dbus --localstatedir=/var \
+    --disable-manual-pages --disable-pie --disable-shared --enable-static
+
+sed 's/$(LDFLAGS) -o/-all-static $(LDFLAGS) -o/' Makefile > _
+mv -f _ Makefile
+make -j($nproc)
+make install
+make distclean
