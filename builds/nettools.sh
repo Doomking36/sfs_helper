36f73a949935fc57e661b2c871d1009365abed14
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:45:07 -0600

    Update and rename template.sh to nettools.sh

---
+++ builds/nettools.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd /mnt/src/sources/net-tools/
+
+env CC="/mnt/src/gcc-toolchain/bin/gcc -L/mnt/lib -I/mnt/include -w" \
+    LDFLAGS="-s -static -g0" CFLAGS="-Os -pipe -march=native" \
+    AR="/mnt/src/gcc-toolchain/bin/ar" RANLIB="/mnt/src/gcc-toolchain/bin/ranlib" \
+    make -j2 #configure what you need
+
+env CC="/mnt/src/gcc-toolchain/bin/gcc -L/mnt/lib -I/mnt/include -w" \
+    LDFLAGS="-s -static -g0" CFLAGS="-Os -pipe -march=native" \
+    AR="/mnt/src/gcc-toolchain/bin/ar" RANLIB="/mnt/src/gcc-toolchain/bin/ranlib" \
+    make -j2
+
+cd /mnt/src;
+chroot /mnt /bin/csh #comming back to the action!!
+source /etc/profile
+cd /src/sources/net-tools
+
+make -e -j($nproc)
+
+make -e install DESTDIR="./NETTOOLS"
+mv ./NETTOOLS/bin/* /bin/
+mv ./NETTOOLS/sbin/* /bin/
+
+rm -r ./NETTOOLS/
+make clean
