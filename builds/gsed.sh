dea05b77d5eb4126fb37af398c8337cf144c2261
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:13:05 -0600

    Update and rename template.sh to gsed.sh

---
+++ builds/gsed.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+env CC="/mnt/bin/tcc -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" ./configure --prefix=/ --disable-nls --disable-rpath \
+    --without-selinux
+
+make -j($nproc)
+make DESTDIR="./GSED/" install
+mv GSED/bin/sed /mnt/bin/gsed
+mv GSED/share/man/man1/sed.1 /mnt/share/man/man1/gsed.1
+
+rm -r ./GSED
+make distclean
