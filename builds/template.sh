e0a841177d0a9d32e543ba55128c419795f96134
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 01:22:56 -0600

    Update template.sh

--- builds/template.sh
+++ builds/template.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
 
+env CFLAGS="-Os -pipe -march=native" \
+    CC="/mnt/src/gcc-toolchain/bin/gcc" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/src/gcc-toolchain/bin/ar"  \
+    RANLIB="/mnt/src/gcc-toolchain/bin/ranlib" \
+./configure --prefix="/" --target=x86_64 \
+            --syslibdir="/lib" --bindir="/bin" \
+            --includedir="/include" \
+            --disable-shared --enable-static
+
+# Rule of thumb -j# where # is halve the RAM to avoid OOM
+make -j($nproc)
+make DESTDIR="/mnt/" install
+make distclean
