160a5cc3886ba26410124f759325c4c84ce4b464
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:15:02 -0600

    Update and rename template.sh to linux_headers.sh

---
+++ builds/linux_headers.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ln -sf /mnt/bin/gsed ./sed
+
+make PATH="./:$PATH" ARCH="x86" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -Os -pipe -march=native -g0 -s -static -w" headers -j($nproc)
+
+find usr/include/ -name \*.h -type f | while read -r file;
+do f="${file%/*}"
+   f="${f#usr}"
+   mkdir -p -v /mnt$f/
+   cp -f -v $file /mnt$f/
+done
+
+make CC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" ARCH="x86" distclean -j2
+rm ./sed
