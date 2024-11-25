39293f918ba6788c7ea6478684dceeaf89630171
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:07:33 -0600

    Update sbase.sh

--- builds/sbase.sh
+++ builds/sbase.sh
@@ -3,7 +3,7 @@
 env CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
-    RANLIB="echo" make -e -j2
+    RANLIB="echo" make -e -j($nproc)
 make -e PREFIX="/" DESTDIR="/mnt/" install
 ln -sf xinstall /mnt/bin/install
 make clean
