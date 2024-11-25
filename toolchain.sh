5e794e7c786bc64eaf8beb131cdcc711d85a80ca
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 00:57:36 -0600

    Update and rename template.sh to toolchain.sh

---
+++ toolchain.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -s musl.cc | grep x86_64-linux-musl-native | xargs curl '-#' -fo /mnt/src/packages/x86_64-linux-musl-native.tgz \
+&& mkdir -p /mnt/src/gcc-toolchain/ \
+&& tar -xzf /mnt/src/packages/x86_64-linux-musl-native.tgz -C/mnt/src/gcc-toolchain/ --strip-components=1
