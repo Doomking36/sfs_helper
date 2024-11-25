fe34e4220050e614f28232370eadb08da1f0b4c2
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 00:19:52 -0600

    Update and rename template.sh to create_dir.sh

---
+++ create_dir.sh
@@ -0,0 +1,14 @@
+mkdir -p                  \
+      /mnt/proc           \
+      /mnt/dev            \
+      /mnt/sys            \
+      /mnt/etc/ssl/certs  \
+      /mnt/tmp            \
+      /mnt/var/run        \
+      /mnt/root           \
+      /mnt/lib            \
+      /mnt/include        \
+      /mnt/bin            \
+      /mnt/src            \
+      /mnt/src/packages   \
+      /mnt/src/sources
