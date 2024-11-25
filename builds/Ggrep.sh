b911e83b001cd1a0c1e6904d8c08071d7b2890aa
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:13:47 -0600

    Update and rename template.sh to Ggrep.sh

---
+++ builds/Ggrep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+env CC="/mnt/bin/tcc -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" ./configure --prefix=/ --disable-nls \
+    --disable-rpath --enable-threads=posix --without-gnu-ld
+
+make -j($nproc)
+make DESTDIR="../GGREP/" install
+
+mv GGREP/bin/grep /mnt/bin/ggrep
+mv GGREP/share/man/man1/grep.1 /mnt/share/man/man1/ggrep.1
+rm -r ./GGREP/
+
+make distclean
