23a5cda86d3f5dc77adc742d78aa60911cb99001
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 01:26:07 -0600

    Update and rename template.sh to tinycc.sh

---
+++ builds/tinycc.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+env CFLAGS="-Os -pipe -march=native" \
+    CC="/mnt/src/gcc-toolchain/bin/gcc" \
+    LDFLAGS="-s -static -g0 -L/mnt/lib -I/mnt/include -w" \
+    AR="/mnt/src/gcc-toolchain/bin/ar"  \
+    RANLIB="/mnt/src/gcc-toolchain/bin/ranlib" \
+./configure --config-musl --disable-rpath --enable-static \
+--prefix=/ --sysincludepaths="/mnt/lib/tcc/include:/mnt/include:/lib/tcc/include:/include" \
+--libpaths="/mnt/lib:/mnt/lib/tcc:/lib:/lib/tcc" --crtprefix="/mnt/lib:/lib" \
+--cross-prefix="/mnt" --cc="/src/gcc-toolchain/bin/gcc -L/mnt/lib -I/mnt/include -w -s -static -g0" \
+--ar="/src/gcc-toolchain/bin/ar"
+
+make -j($nproc)
+make DESTDIR="/mnt" install
+make distclean
