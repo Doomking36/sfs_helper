3ddeaf992cdee23b7bab2729b545b70823507f06
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:21:13 -0600

    Update and rename template.sh to xz.sh

---
+++ chroot/xz.sh
@@ -0,0 +1,12 @@
+#!/bin/csh
+
+env LD="echo" ./configure --prefix=/ --disable-doc --disable-microlzma \
+        --disable-lzip-decoder --enable-small --enable-threads=posix \
+        --disable-lzmadec --disable-lzmainfo --disable-lzma-links \
+        --disable-scripts --enable-sandbox=no --enable-static \
+        --disable-nls --disable-rpath --without-gnu-ld --disable-shared \
+        --without-pic
+
+make LDFLAGS="-s -static -g0 -all-static" -j($nproc)
+make install
+make distclean
