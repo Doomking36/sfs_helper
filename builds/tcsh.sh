cdce81b195b52b3b7d6193533826702e5612bec2
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 02:10:30 -0600

    Update and rename template.sh to tcsh.sh

---
+++ builds/tcsh.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+env CC="/mnt/bin/tcc -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" LIBS="-lcurses -lterminfo" ./configure --disable-rpath \
+    --disable-nls --disable-nls-catalogs --prefix="/" --without-gnu-ld
+
+make -j($nproc)
+make DESTDIR="/mnt/" install
+ln -sf tcsh /mnt/bin/csh
+make clean
+
+#load /etc/profile script
+cat << . > /mnt/root/.cshrc
+source /etc/profile
+.
