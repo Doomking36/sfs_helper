e252fdc922c474530ffaee4a1b7e0269d6766c0e
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 01:02:48 -0600

    Update ggrep.sh

--- builds/ggrep.sh
+++ builds/ggrep.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /mnt/src/sources/grep/
+
 env CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
@@ -14,3 +16,5 @@ mv GGREP/share/man/man1/grep.1 /mnt/share/man/man1/ggrep.1
 rm -r ./GGREP/
 
 make distclean
+
+cd -
