89b9def49420e3c73f60c461c6d3af54d3807622
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:59:31 -0600

    Update byacc.sh

--- builds/byacc.sh
+++ builds/byacc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /mnt/src/sources/byacc*
+
 env CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
@@ -8,3 +10,5 @@ env CC="/mnt/bin/tcc -s -static -g0" \
 make -j$(nproc)
 make DESTDIR="/mnt/" install
 make distclean
+
+cd -
