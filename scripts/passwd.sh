e9b6ea891d63e2e1e72ad92a42bd15e1dc509943
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-25 20:52:29 -0600

    Update and rename passwd to passwd.sh

--- scripts/passwd.sh
+++ scripts/passwd.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
+
+cat > passwd <<EOF
 root:x:0:0:root:/root:/bin/csh
 bin:x:1:1:bin:/dev/null:/bin/false
 //alpine: 
 //bin:x:1:1:bin:/bin:/sbin/nologin
 //daemon:x:2:2:daemon:/sbin:/sbin/nologin
 //shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
+EOF
+
+mv passwd /mnt/etc/
