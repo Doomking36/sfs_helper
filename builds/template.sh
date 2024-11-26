35e291404f3c1569b83cd06ac44253d50fc00762
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 02:13:32 -0600

    Update template.sh

--- builds/template.sh
+++ builds/template.sh
@@ -1,1 +1,19 @@
+#!/bin/sh -e
 
+./musl.sh
+./tinycc.sh
+./tcc_tinycc.sh
+./musl_tcc_tinycc.sh
+./sbase.sh
+./ubase.sh
+./dash.sh
+./nasm.sh
+./e3.sh
+./netbsd-curses.sh
+./tcsh.sh
+./make.sh
+./byacc.sh
+./awk.sh
+./gsed.sh
+./ggrep.sh
+./linux-headers.sh
