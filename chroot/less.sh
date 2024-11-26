97bd05c44aaa01f7d1c5c3798a7d6cd7b6e339cb
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:46:29 -0600

    Update less.sh

--- chroot/less.sh
+++ chroot/less.sh
@@ -4,6 +4,6 @@ env LIBS="-lcurses" LD="echo" CC="tcc -s -static -g0" \
     ./configure --prefix=/ --with-no-float --with-editor=vim \
     --with-regex=posix
 
-make -j($nproc)
+make -j$(nproc)
 make install
 make distclean
