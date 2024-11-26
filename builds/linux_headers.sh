f6f32c2a6f79cc5df300a74d5ac8a9da64f20d83
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:41:40 -0600

    Update linux_headers.sh

--- builds/linux_headers.sh
+++ builds/linux_headers.sh
@@ -2,7 +2,7 @@
 
 ln -sf /mnt/bin/gsed ./sed
 
-make PATH="./:$PATH" ARCH="x86" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -Os -pipe -march=native -g0 -s -static -w" headers -j($nproc)
+make PATH="./:$PATH" ARCH="x86" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -Os -pipe -march=native -g0 -s -static -w" headers -j$(nproc)
 
 find usr/include/ -name \*.h -type f | while read -r file;
 do f="${file%/*}"
@@ -11,5 +11,5 @@ do f="${file%/*}"
    cp -f -v $file /mnt$f/
 done
 
-make CC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" ARCH="x86" distclean -j2
+make CC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" HOSTCC="/mnt/src/gcc-toolchain/bin/gcc -s -static -g0" ARCH="x86" distclean -j$(nproc)
 rm ./sed
