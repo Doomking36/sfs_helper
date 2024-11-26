4fc168a95131ea4e01fbbc4e7b2c0b626860bbe0
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:42:25 -0600

    Update make.sh

--- builds/make.sh
+++ builds/make.sh
@@ -6,6 +6,6 @@ env CC="/mnt/bin/tcc -s -static -g0" \
     RANLIB="echo" LD="echo" ./configure --prefix="/" --disable-nls --disable-rpath \
     --bindir=/bin --sbindir=/bin --libexecdir=/bin --without-gnu-ld
 
-make -j($nproc)
+make -j$(nproc)
 make DESTDIR="/mnt/" install
 make distclean
