1f1755c5c9ec480968bd657903020c690caf31dc
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:56:58 -0600

    Update awk.sh

--- builds/awk.sh
+++ builds/awk.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+cd /mnt/src/awk*/
+
 env HOSTCC="/mnt/bin/tcc -s -static -g0" \
     AR="/mnt/bin/tcc -ar" RANLIB="echo" YACC="/mnt/bin/yacc -d -b awkgram" \
     
 make -e -j$(nproc)
 mv ./a.out /mnt/bin/awk
 make distclean
+
+cd -
