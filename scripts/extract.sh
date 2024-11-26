6bfec68f14c0d7672c8b6866c89a84db90d49869
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:35:57 -0600

    Update and rename template.sh to extract.sh

---
+++ scripts/extract.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Search through directory to extract and delete .gz, .xz, and .zip 
+find . \( -name "*.tar.gz" -o -name "*.tar.xz" -o -name "*.zip" \) -exec sh -c '
+  case "$1" in
+    *.tar.gz|*.tar.xz) tar xf "$1" ;;
+    *.zip) unzip "$1" ;;
+  esac && rm "$1"
+' sh {} \;
