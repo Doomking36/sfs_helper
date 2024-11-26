9cff0f7a64ace282be181a5c494b0cb128d9d296
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 01:13:47 -0600

    Update xz.sh

--- chroot/xz.sh
+++ chroot/xz.sh
@@ -1,5 +1,7 @@
 #!/bin/csh
 
+cd /src/sources/xz/
+
 env LD="echo" ./configure --prefix=/ --disable-doc --disable-microlzma \
         --disable-lzip-decoder --enable-small --enable-threads=posix \
         --disable-lzmadec --disable-lzmainfo --disable-lzma-links \
@@ -10,3 +12,5 @@ env LD="echo" ./configure --prefix=/ --disable-doc --disable-microlzma \
 make LDFLAGS="-s -static -g0 -all-static" -j$(nproc)
 make install
 make distclean
+
+cd -
