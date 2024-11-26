5221bcf30f15980aab4c1a21dcc6fbc27548adf7
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-26 00:40:33 -0600

    Update dash.sh

--- builds/dash.sh
+++ builds/dash.sh
@@ -6,7 +6,7 @@ env CC="/mnt/bin/tcc -s -static -g0" \
     RANLIB="echo" ./configure --prefix="/" --disable-fnmatch \
     --disable-lineno --disable-glob
 
-make -j($nproc)
+make -j$(nproc)
 make DESTDIR="/mnt/" install
 mv /mnt/bin/dash /mnt/bin/sh
 make distclean
