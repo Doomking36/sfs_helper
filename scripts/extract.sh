3055243d69ea8526fe42025a42d070488ae73581
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-28 01:08:04 -0600

    Update extract.sh

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -2,10 +2,10 @@
 
 cd /mnt/src/packages
 
-# Search through directory to extract and delete .gz, .xz, and .zip 
-find . \( -name "*.tar.gz" -o -name "*.tar.xz" -o -name "*.zip" \) -exec sh -c '
+# Search through directory to extract and delete .gz, .xz, .zip, and .tgz
+find . \( -name "*.tar.gz" -o -name "*.tar.xz" -o -name "*.zip" -o -name "*.tgz" \) -exec sh -c '
   case "$1" in
-    *.tar.gz|*.tar.xz) tar xf "$1" ;;
+    *.tar.gz|*.tar.xz|*.tgz) tar xf "$1" ;;
     *.zip) unzip "$1" ;;
   esac && rm "$1"
 ' sh {} \;
