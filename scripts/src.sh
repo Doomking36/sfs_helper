340e1ca24b29601a62529cff82ddb6377a9d57de
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-28 00:31:49 -0600

    Update src.sh

--- scripts/src.sh
+++ scripts/src.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
-curl -LO https://git.disroot.org/nyght/sfs_sources/src/branch/main/sources.sh \
-&& mv sources.sh /mnt/src/ \
-&& /mnt/src/./sources.sh
+curl -LO https://git.disroot.org/nyght/sfs_sources/src/branch/main/sources.sh
+mv sources.sh /mnt/src/ 
+cd /mnt/src/
+./sources.sh
+cd -
