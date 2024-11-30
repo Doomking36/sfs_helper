c35cd94ca32f2d868b57bd6e8fb329de65c4ea5f
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:39:55 -0600

    Update netbsd-curses.sh

--- builds/netbsd-curses.sh
+++ builds/netbsd-curses.sh
@@ -6,6 +6,7 @@ sed '514,517d' libcurses/color.c > _
 mv -f _ libcurses/color.c
 
 env CC="/mnt/bin/tcc -s -static -g0 -w" \
+    CFLAGS="-Os -march=native -pipe -s -static -g0" \
     AR="/mnt/src/gcc-toolchain/bin/ar" \
     LDFLAGS="-s -static -g0" make -j$(nproc) all-static
 
