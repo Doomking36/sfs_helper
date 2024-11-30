48eae49e72596a027e0994c943c79d905c930516
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:42:32 -0600

    Update vim.sh

--- builds/vim.sh
+++ builds/vim.sh
@@ -3,6 +3,7 @@
 cd /mnt/src/sources/vim/
 
 env CC="/mnt/bin/tcc -s -static -g0" \
+    CFLAGS="-Os -march=native -pipe -s -static -g0" \
     LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" ./configure --prefix="/" --enable-cscope --enable-multibyte --with-ex-name=vim-ex --with-view-name=vim-view --disable-perlinterp \
