1e317d547710245a30db480dd97e8e3f65c0565a
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 03:47:34 -0600

    Update and rename template.sh to vim.sh

---
+++ builds/vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+env CC="/mnt/bin/tcc -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" ./configure --prefix="/" --enable-cscope --enable-multibyte --with-ex-name=vim-ex --with-view-name=vim-view --disable-perlinterp \
+    --disable-pythoninterp --disable-rubyinterp --disable-netbeans --disable-gpm --disable-xim --disable-gui --disable-nls --without-x
+
+make -j$(nproc)
+make DESTDIR="/mnt" install
+make distclean
