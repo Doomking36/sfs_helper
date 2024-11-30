712ca4855ccba2431adf815fcc89ff66d395f23d
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:44:31 -0600

    Update nasm.sh

--- builds/nasm.sh
+++ builds/nasm.sh
@@ -3,8 +3,8 @@
 cd /mnt/src/packages/nasm*/
 
 env CC="/mnt/bin/tcc -s -static -g0"   \
-    LDFLAGS="-s -static -g0" \
     CFLAGS="-Os -march=native -pipe -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" ./configure --prefix=/
 
