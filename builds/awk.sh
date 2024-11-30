deb13cc35cc480865c4c48373d1effdec2bb2497
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 05:22:46 -0600

    Update awk.sh

--- builds/awk.sh
+++ builds/awk.sh
@@ -2,13 +2,13 @@
 
 cd /mnt/src/sources/awk/
 
-env HOSTCC="/mnt/bin/tcc -s -static -g0" \
+env HOSTCC="/mnt/bin/tcc -static" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" \
     YACC="/mnt/bin/yacc -d -b awkgram" \
-    CFLAGS="-Os -march=native -pipe" \
-    LDFLAGS="-s -static" \
-    CC="/mnt/bin/tcc" \
+    CFLAGS="-Os -march=native -pipe -static" \
+    LDFLAGS="-static" \
+    CC="/mnt/bin/tcc -static" \
     make -e -j$(nproc)
 
 strip -s ./a.out
