3ac120ba48a98083f42ade26c164356cea080744
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 05:01:26 -0600

    Update awk.sh

--- builds/awk.sh
+++ builds/awk.sh
@@ -3,9 +3,15 @@
 cd /mnt/src/sources/awk/
 
 env HOSTCC="/mnt/bin/tcc -s -static -g0" \
-    AR="/mnt/bin/tcc -ar" RANLIB="echo" YACC="/mnt/bin/yacc -d -b awkgram" \
-    
-make -e -j$(nproc)
+    AR="/mnt/bin/tcc -ar" \
+    RANLIB="echo" \
+    YACC="/mnt/bin/yacc -d -b awkgram" \
+    CFLAGS="-Os -march=native -pipe" \
+    LDFLAGS="-s -static" \
+    CC="/mnt/bin/tcc" \
+    make -e -j$(nproc)
+
+strip -s ./a.out
 mv ./a.out /mnt/bin/awk
 make distclean
 
