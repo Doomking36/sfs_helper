adeafefa3d9b7a0a68107fd3f66b153b173c0bc9
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:42:15 -0600

    Update ubase.sh

--- builds/ubase.sh
+++ builds/ubase.sh
@@ -5,6 +5,7 @@ cd /mnt/src/sources/ubase/
 env CPPFLAGS="-D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE" \
     CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
+    CFLAGS="-Os -march=native -pipe -s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" make -e -j$(nproc)
     
