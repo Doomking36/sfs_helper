2cc88ba59e36cb8ce9df3b384903dcd24a85af25
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 03:56:29 -0600

    Update create_dir.sh

--- scripts/create_dir.sh
+++ scripts/create_dir.sh
@@ -18,7 +18,7 @@ mkdir -p                  \
 mount --bind /proc /mnt/proc
 #mount --bind /dev /mnt/dev
 mount --bind /sys /mnt/sys
-mount --bind /etc/ssl/certs /mnt/etc/ssl/certs
+#mount --bind /etc/ssl/certs /mnt/etc/ssl/certs
 mount --bind /tmp /mnt/tmp
 mount --bind /var/run /mnt/var/run
 mount --bind /lib /mnt/lib
