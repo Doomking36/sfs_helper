bf93dab9a88c67937c136651747dd3e9ad8c4a2b
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:43:13 -0600

    Update tcsh.sh

--- builds/tcsh.sh
+++ builds/tcsh.sh
@@ -3,8 +3,8 @@
 cd /mnt/src/packages/tcsh*/
 
 env CC="/mnt/bin/tcc -s -static -g0" \
-    LDFLAGS="-s -static -g0" \
     CFLAGS="-Os -march=native -pipe -s -static -g0" \
+    LDFLAGS="-s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" LIBS="-lcurses -lterminfo" ./configure --disable-rpath \
     --disable-nls --disable-nls-catalogs --prefix="/" --without-gnu-ld
