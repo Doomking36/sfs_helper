9025f2ec197fb9d57d5e206bf3a605896b97fd0b
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:40:17 -0600

    Update sbase.sh

--- builds/sbase.sh
+++ builds/sbase.sh
@@ -4,6 +4,7 @@ cd /mnt/src/sources/sbase/
 
 env CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
+    CFLAGS="-Os -march=native -pipe -s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" make -e -j$(nproc)
     
