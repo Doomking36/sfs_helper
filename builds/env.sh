8aadb27446e966545abc606456de8533332abbaf
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 15:03:46 -0600

    Update and rename template.sh to env.sh

---
+++ builds/env.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e 
+
+# cd /mnt/src/sources/net-tools/
+
+env CC="/mnt/src/gcc-toolchain/bin/gcc -L/mnt/lib -I/mnt/include -w" \
+    LDFLAGS="-s -static -g0" CFLAGS="-Os -pipe -march=native" \
+    AR="/mnt/src/gcc-toolchain/bin/ar" RANLIB="/mnt/src/gcc-toolchain/bin/ranlib" \
+    make -j2
+
+# Run env.sh once more
+
+# cd /mnt/src
+# chroot /mnt /bin/csh
+# source /etc/profile
+# cd /src/sources/net-tools
+
+# make -e -j2
+# make -e install DESTDIR="./NETTOOLS"
+# mv ./NETTOOLS/bin/* /bin/
+# mv ./NETTOOLS/sbin/* /bin/
+
+# rm -r ./NETTOOLS/
+# make clean
