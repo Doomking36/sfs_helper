74e2a75647e6c10c6203c67680ede9d51187edc7
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:46:55 -0600

    Update awk.sh

--- builds/awk.sh
+++ builds/awk.sh
@@ -6,9 +6,9 @@ env HOSTCC="/mnt/bin/tcc -s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" \
     YACC="/mnt/bin/yacc -d -b awkgram" \
+    CC="/mnt/bin/tcc -s -static -g0" \
     CFLAGS="-Os -march=native -pipe -s -static -g0" \
     LDFLAGS="-s -static -g0" \
-    CC="/mnt/bin/tcc -s -static -g0" \
     make -e -j$(nproc)
 
 strip -s ./a.out
