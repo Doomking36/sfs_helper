4edd99fca432b91d39ee60b8bca4b05b926dd67d
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-11-30 12:36:14 -0600

    Update make.sh

--- builds/make.sh
+++ builds/make.sh
@@ -4,6 +4,7 @@ cd /mnt/src/packages/make*/
 
 env CC="/mnt/bin/tcc -s -static -g0" \
     LDFLAGS="-s -static -g0" \
+    CFLAGS="-Os -march=native -pipe -s -static -g0" \
     AR="/mnt/bin/tcc -ar" \
     RANLIB="echo" LD="echo" ./configure --prefix="/" --disable-nls --disable-rpath \
     --bindir=/bin --sbindir=/bin --libexecdir=/bin --without-gnu-ld
