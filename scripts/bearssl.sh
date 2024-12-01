805f12bb6e360b145b25af3b5fb95ec3eed1e169
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-01 02:56:52 -0600

    Update bearssl.sh

--- scripts/bearssl.sh
+++ scripts/bearssl.sh
@@ -2,3 +2,6 @@
 
 sed 's/cc/tcc/' /mnt/src/sources/BearSSL/conf/Unix.mk > _
 mv -f _ /mnt/src/sources/BearSSL/conf/Unix.mk
+
+sed 's/gcc/tcc/' /mnt/src/sources/BearSSL/Makefile > _
+mv -f _ /mnt/src/sources/BearSSL/Makefile
