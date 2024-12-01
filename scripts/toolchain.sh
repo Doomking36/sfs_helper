18e3e284a9b2735c226a068a193a50dba0cc6b33
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-01 04:15:25 -0600

    Update toolchain.sh

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-curl -s musl.cc | grep x86_64-linux-musl-native | xargs curl '-#' -fo /mnt/src/packages/x86_64-linux-musl-native.tgz \
-&& mkdir -p /mnt/src/gcc-toolchain/ \
-&& tar -xzf /mnt/src/packages/x86_64-linux-musl-native.tgz -C/mnt/src/gcc-toolchain/ --strip-components=1
+curl -s musl.cc | grep x86_64-linux-musl-native | xargs curl '-#' -fo /mnt/src/packages/x86_64-linux-musl-native.tgz 
+mkdir -p /mnt/src/gcc-toolchain/ 
+tar -xzf /mnt/src/packages/x86_64-linux-musl-native.tgz -C /mnt/src/gcc-toolchain/ --strip-components=1
