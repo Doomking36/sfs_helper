51fe930f84d689bd169e3b8ca955e2a1f3cc0726
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-05 01:17:57 -0600

    Update and rename template.sh to cproc.sh

---
+++ builds/cproc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+cd /mnt/src/sources/cproc/
+
+env CC="/mnt/bin/tcc -s -static -g0 -L/mnt/lib/ -I/mnt/include" \
+    LDFLAGS="-s -static -g0" \
+    AR="/mnt/bin/tcc -ar"  \
+    RANLIB="echo" \
+./configure --prefix=/
+
+make -j$(nproc)
+make DESTDIR="/mnt" install
+make distclean
