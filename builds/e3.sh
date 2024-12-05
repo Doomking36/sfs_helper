cf5016a1a720740685d259bf4a9f39618bfa5104
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2024-12-05 01:20:22 -0600

    Update e3.sh

--- builds/e3.sh
+++ builds/e3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-cd /mnt/src/packages/e3*/
+cd /mnt/src/sources/e3/
 
 sed 's/nasm\ -O2/\/mnt\/bin\/nasm\ -O2/' Makefile > _
 mv -f _ Makefile
@@ -10,5 +10,3 @@ make -e PREFIX="/mnt/" MANDIR="/mnt/share/man/man1/" install
 for i in /mnt/bin/e3*[em,ne,pi,vi,ws]; do ln -sf e3 $i; done
 
 make clean
-
-cd -
